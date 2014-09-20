5ce00289f9aab077e5156d572ecb855bb5b5f103
jbf034 <jbf034@126>
2014-09-20 19:00:16 +0800

    wei xin init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weixin::Application.config.secret_key_base = 'dcbf751aa44dda298c037d140550e1582474f7439e4f89927b50a315440f7e6cec3b8d69edc8069b90e920166d5841bef3343bbd36725dbc986496624858ce97'
